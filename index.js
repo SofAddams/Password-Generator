db492a9d16ef0f3793a317ac0b9e16ff0c30a72e
Sof Addams <thesofdesign@proton.me>
2026-01-12 00:11:27 +0100

    Add personal comments about coding journey
    Added comments reflecting personal thoughts and aspirations about coding and AI.

--- index.js
+++ index.js
@@ -50,3 +50,9 @@ secondPassword.addEventListener("click", function () {
 });
 
 // Comment to try GitHub commit in VSCode
+// Comment that probably no one will see anyway, To get that dumb green check, and I'm hesitating to follow the coding path, I think I'll get back to Scrimba and learn coding to be honest.
+// I've been hesitating between this or focus fully on design, but I want to be able to build my own products, focusing 3 things I love: drawing/design/building my own things
+// That's only possible if I learn how to code and then use AI to enhance myself, I don't want to have it as a crutch, where I can't do shit without it. 
+// The world with the growth of AI is becoming increasigly hard to fathom too, making it hard to make a decision, guess I have to jump, we'll see if I land in hell or heaven. 
+
+// That's it, that was Sof, 12/01/2026, and have a new year/good life ofc if you're reading this, whishing you the best ;)
